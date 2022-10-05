37347863ef1c07d0916dd48f48b6e6c7adf42f2f
alexdev22 <alejandro2210lp@gmail.com>
2022-10-05 14:19:26 +0200

    Improved styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,16 @@ import ErrorPage from './pages/ErrorPage'
 import IndividualProyectInfo from './pages/IndividualProyectInfo'
 import Menu from './components/Menu'
 import Landing from './pages/Landing'
+import { useEffect } from 'react'
 
 function App () {
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const introImg = document.querySelector('.intro__img')
+      const positionY = window.scrollY
+      introImg.style.transform = (`translateY(${positionY * 0.05}px)`)
+    })
+  }, [])
   return (
 
     <BrowserRouter>
