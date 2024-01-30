7b764d2451cc27881d3807330b0597de30104a29
alejandrocoder22 <alejandro2210lp@gmail.com>
2024-01-30 19:15:11 +0100

    Added current year with javascript

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import { BsGithub, BsLinkedin } from 'react-icons/Bs'
+
+const currentYear = new Date().getFullYear()
 const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer__wrapper'>
-        <span>Alejandrocoder &copy; 2022</span>
+        <span>Alejandrocoder &copy; {currentYear}</span>
         <div className='footer__icons'>
           <a href='https://github.com/alejandrocoder22/' target='_blank' rel='noreferrer noopener'> <BsGithub className='footer__svg ' /> </a>
           <a href='https://linkedin.com/in/alejandrocoder' target='_blank' rel='noreferrer noopener'>   <BsLinkedin className='footer__svg mml' /> </a>
